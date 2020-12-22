c5f9dad7d2f531bdfa62ef8cb6a5cb7e8e7b197d
Lokesh Kumar <neolokeshkr@gmail.com>
2020-12-22 18:20:25 +0530

    Sample Dashboard with table created.

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get("/",function(req,res){
     res.render('index')
 });
 
+app.get("/dashboard",function(req,res){
+    res.render('dashboard')
+});
+
 //Server Configuration
 app.listen("3000",function(){
     console.log("Server started at port 3000!")
