8642a95d4e6fec52a29b3f0818a5e05fe6d6e40a
Lokesh Kumar <neolokeshkr@gmail.com>
2020-12-24 13:48:15 +0530

    Some Styling added

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ app.get("/",function(req,res){
     res.render('index')
 });
 
-app.get("/dashboard",function(req,res){
-    res.render('dashboard')
+app.get("/products",function(req,res){
+    res.render('products')
+});
+
+app.get("/productsHistory",function(req,res){
+    res.render('productsHistory')
 });
 
 //Server Configuration
